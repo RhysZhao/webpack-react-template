e7fc247514fb5d2d6f4374cd0e508d8fdde9b5ce
rhys.zhao <zhaorx2012@163.com>
2023-01-29 14:15:18 +0800

    Update 删除无用代码

--- config/env.config.js
+++ config/env.config.js
@@ -2,8 +2,8 @@
  * Author  rhys.zhao
  * Date  2022-08-16 16:25:12
  * LastEditors  rhys.zhao
- * LastEditTime  2022-08-16 16:27:28
- * Description
+ * LastEditTime  2023-01-29 14:14:27
+ * Description  全局变量配置
  */
 !(() => {
   window.ENV = {
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,14 +2,14 @@
  * Author  rhys.zhao
  * Date  2022-08-19 17:48:33
  * LastEditors  rhys.zhao
- * LastEditTime  2023-01-29 10:41:17
+ * LastEditTime  2023-01-29 14:14:44
  * Description home页面
  */
 import React from "react";
 import styles from "./index.scss";
 
 const Home = () => {
-  return <div className={styles.home}>这里是Home页面55999</div>;
+  return <div className={styles.home}>这里是Home页面</div>;
 };
 
 export default Home;
